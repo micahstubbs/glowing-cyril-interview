30ca6ec838a7a679bbd938f75c3345f2ea0e20c7
micahstubbs <micah.stubbs@gmail.com>
2013-07-01 23:41:04 -0500

    belatedly began writing rspec tests

--- spec/factories/trades.rb
+++ spec/factories/trades.rb
@@ -1,6 +1,21 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :trade do
+  factory :trade do |f|
+  	f.counterparty { "NYMEX" }
+    f.month_id { 2 }
+    f.curve_id { 3 }
+    f.trader { "Bill Smith" }
+    f.mtm_curve { "NYMEX CL" }
+    f.buy_sell { "Buy" }
+    f.price { 75 }
+    f.volume { 10000 } 
+    f.tenor_start { "Apr-13" } 
+    f.tenor_end { "May-13" }
+    f.delivery_location { "" }
+    f.instrument { "NYMEX CL" }
+    f.brokerage { "OTC Global" } 
+    f.tags { "#oil #hedge"}
+    f.notes { "" }
   end
 end
--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
+require 'factory_girl'
 
 describe Trade do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has a valid factory" do
+    FactoryGirl.create(:trade).should be_valid
+  end
+
+  it "is invalid without a volume"
+
+  it "is invalid without a price" 
+  it "returns a trader's name as a string"
+
 end
+
